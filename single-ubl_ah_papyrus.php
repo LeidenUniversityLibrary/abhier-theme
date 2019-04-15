64b7f5fa6bc010cbec7edcec420b40012aeb287e
Ben Companjen <ben@companjen.name>
2019-04-15 14:26:33 +0200

    Incorporate content template part

--- single-ubl_ah_papyrus.php
+++ single-ubl_ah_papyrus.php
@@ -4,11 +4,11 @@
  *
  * @link  https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
- * @package    Modern
- * @copyright  WebMan Design, Oliver Juhas
+ * @package    abhier-theme
+ * @copyright  WebMan Design, Oliver Juhas / Leiden University Libraries, Ben Companjen
  *
  * @since    1.0.0
- * @version  2.0.0
+ * @version  1.4.0
  */
 
 
@@ -20,16 +20,45 @@ get_header();
 	while ( have_posts() ) : the_post();
 
 		/**
-		 * Include the Post-Format-specific template for the content.
-		 * If you want to override this in a child theme, then include a file
-		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-		 *
-		 * Or, you can use the filter hook below to modify which content file to load.
+		 * copied from the content.php template part
 		 */
-		get_template_part(
-			'template-parts/content/content',
-			apply_filters( 'wmhook_modern_single_content_type', get_post_format() )
-		);
+		do_action( 'tha_entry_before' );
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php do_action( 'tha_entry_top' ); ?>
+
+	<div class="entry-content"><?php
+
+		do_action( 'tha_entry_content_before' );
+
+		// if ( Modern_Post::is_singular() ) {
+
+			if ( has_excerpt() && ! Modern_Post::is_paged() ) {
+				the_excerpt();
+			}
+			// Mirador viewer goes between excerpt and the rest of the content.
+			do_action( 'ubl_ah_mirador_viewer' );
+
+			the_content( apply_filters( 'wmhook_modern_summary_continue_reading', '' ) );
+
+		// } else {
+			// the_excerpt();
+		// }
+
+		do_action( 'tha_entry_content_after' );
+
+	?></div>
+
+	<?php do_action( 'tha_entry_bottom' ); ?>
+
+</article>
+
+<?php
+
+do_action( 'tha_entry_after' );
 
 		// If comments are open or we have at least one comment, load up the comment template.
 		if ( comments_open() || get_comments_number() ) {
