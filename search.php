737fbf4be5f5eb665847c1c038bc188056d4205e
Ben Companjen <ben@companjen.name>
2019-06-05 11:21:23 +0200

    Add message about the partial search results

---
+++ search.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying search results pages
+ *
+ * @link  https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ * @uses  `wmhook_modern_title_primary_disable` global hook to disable `#primary` section H1
+ *
+ * @package    Modern
+ * @copyright  WebMan Design, Oliver Juhas
+ *
+ * @since    1.0.0
+ * @version  2.0.0
+ */
+
+
+
+
+
+get_header();
+
+	if ( have_posts() ) :
+
+		if ( ! (bool) apply_filters( 'wmhook_modern_title_primary_disable', false ) ) :
+
+			?>
+
+			<header class="page-header">
+				<h1 class="page-title"><?php
+
+					printf(
+						/* translators: %s: search query. */
+						esc_html__( 'Search Results for: %s', 'modern' ),
+						'<span>' . get_search_query() . '</span>'
+					);
+
+				?></h1>
+				<p>Note: these search results do not include annotations of words. To search the contents of papyri,
+					please use the <a href="">contents search interface</a>.</p>
+			</header>
+
+			<?php
+
+		endif;
+
+		get_template_part( 'template-parts/loop/loop', 'search' );
+
+	else :
+
+		get_template_part( 'template-parts/content/content', 'none' );
+
+	endif;
+
+get_footer();
