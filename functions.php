8f6b3f5ec4eb2c4e30da09e9f8f81895db96f524
Ben Companjen <ben@companjen.name>
2019-07-29 15:13:47 +0200

    Add wordpressEndpoint.js to theme as plugin

--- functions.php
+++ functions.php
@@ -13,7 +13,8 @@ function abhier_theme_enqueue_styles() {
     if (is_single() && 'ubl_ah_papyrus' == get_post_type()) {
         wp_enqueue_style( 'mirador-style', get_stylesheet_directory_uri() . '/assets/js/mirador/css/mirador-combined.css', false, null);
         wp_enqueue_script( 'mirador', get_stylesheet_directory_uri() . '/assets/js/mirador/mirador.js', array ( 'jquery' ), null);
-        wp_enqueue_script( 'mirador-config', get_stylesheet_directory_uri() . '/assets/js/mirador-config.js', array('mirador'), null, true);
+        wp_enqueue_script( 'wp-endpoint', get_stylesheet_directory_uri() . '/assets/js/wordpressEndpoint.js', array('mirador'), null);
+        wp_enqueue_script( 'mirador-config', get_stylesheet_directory_uri() . '/assets/js/mirador-config.js', array('mirador', 'wp-endpoint'), null, true);
     }
 }
 add_action( 'wp_enqueue_scripts', 'abhier_theme_enqueue_styles' );
