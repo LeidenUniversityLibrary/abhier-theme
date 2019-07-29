ff00d5cf55962d26e86b79ff76b2038add4b590e
Ben Companjen <ben@companjen.name>
2019-07-29 16:03:10 +0200

    Comment out my_object
    WordPress localises a script by adding a script element that contains
    the object with values before the localised script.

--- assets/js/mirador-config.js
+++ assets/js/mirador-config.js
@@ -1,11 +1,11 @@
 var mir;
-var my_object = {
-    buildPath: "",
-    manifestUri: "",
-    canvasID: "",
-    endpointUrl: "",
-    token: "",
-}
+// var my_object = {
+//     buildPath: "",
+//     manifestUri: "",
+//     canvasID: "",
+//     endpointUrl: "",
+//     token: "",
+// }
 $(function() {
     mir = Mirador({
         id: "viewer",
