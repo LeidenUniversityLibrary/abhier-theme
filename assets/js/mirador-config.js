2fe91c7395ac1906f00086bdcf8bbe8564ecdf6d
Ben Companjen <ben@companjen.name>
2019-08-20 11:47:58 +0200

    Remove debugging, add indentation

--- assets/js/mirador-config.js
+++ assets/js/mirador-config.js
@@ -26,32 +26,15 @@ $(function() {
         'Rectangle', 'Ellipse', 'Polygon'
         ],
         annotationEndpoint: {
-        name: 'WordPress Endpoint',
-        module: 'WordPressEndpoint',
-        options: {
-            url: my_object.endpointUrl,
-            nonce: my_object.nonce,
-            userid: my_object.userid,
-            username: my_object.username,
-            userrole: my_object.userrole,
-        }
+            name: 'WordPress Endpoint',
+            module: 'WordPressEndpoint',
+            options: {
+                url: my_object.endpointUrl,
+                nonce: my_object.nonce,
+                userid: my_object.userid,
+                username: my_object.username,
+                userrole: my_object.userrole,
+            }
         }
     });
-    if (false) {
-        console.log('before emit');
-        console.log(mir.viewer);
-        console.log(mir.eventEmitter);
-        //var wid = mir.viewer.workspace.slots[0].window.id;
-        var wid = "the_window";
-        mir.eventEmitter.subscribe('annotationListLoaded.the_window', function(event) { console.log('received annolist loaded'); });
-        mir.eventEmitter.subscribe('fitBounds.the_window', function(event, bounds) { console.log('received fitBounds'); });
-        mir.eventEmitter.subscribe('imageBoundsUpdated', function(event, data) { 
-            $.map(data.osdBounds, function(item, i) { 
-            console.log('new bounds: ' + item); 
-            }) 
-        });
-        console.log(wid);
-        mir.eventEmitter.publish('fitBounds.'+wid, {'x': 0, 'y': 0, 'width': 200, 'height': 200});
-        console.log('after emit');
-    }
 });
